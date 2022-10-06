52de1b6a26f3ee5f749e7d174ff8392535cb46c7
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-06 17:43:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c17-l3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-govzwr)
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <div className="flex-fill">
+        <strong> React-router </strong>
+      </div>
+      <ul className={styles.headerList}>
+        <Link to="/">Homepage</Link>
+        <Link to="/profile">Profile</Link>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
